091f8fb8bad7a1468a23a690506e80dd5c48fafa
kangddong <rkdehddud96@naver.com>
2022-03-23 23:48:46 +0900

    야근 죽겠다 ...

--- Chapter01/Chapter01/ViewController.swift
+++ Chapter01/Chapter01/ViewController.swift
@@ -5,15 +5,21 @@
 //  Created by 강동영 on 2022/03/21.
 //
 
+/*
+ 화면을 구성하는데 사용되는 세 가지 주요 객체
+ UIScreen 기기에 연결되는 물리적인 화면을 정의하는 객체
+ UIWindow 화면 그리기 지원 도구를 제공하는 객체
+ UIView 그리기를 수행할 객체 세트
+ */
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
