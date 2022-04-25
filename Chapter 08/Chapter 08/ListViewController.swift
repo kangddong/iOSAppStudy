51fdae384fee71cd46a517eeb5aaa1b517d6ee60
kangddong <rkdehddud96@naver.com>
2022-04-26 01:19:28 +0900

    챕터8

--- Chapter 08/Chapter 08/ListViewController.swift
+++ Chapter 08/Chapter 08/ListViewController.swift
@@ -35,32 +35,6 @@ class ListViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //첫번쨰 행
-        var mvo = MoviewVO()
-        mvo.title = "다크나이트"
-        mvo.description = "영웅물에 철학에 음악까지 더해져 예술이 되다."
-        mvo.opendate = "2008-09-04"
-        mvo.rating = 8.95
-        
-        self.list.append(mvo)
-        
-        //두번쨰 행
-        mvo = MoviewVO()
-        mvo.title = "호우시절"
-        mvo.description = "때를 알고 내리는 좋은 비"
-        mvo.opendate = "2008-10-08"
-        mvo.rating = 7.31
-        
-        self.list.append(mvo)
-        
-        //세번쨰 행
-        mvo = MoviewVO()
-        mvo.title = "말할 수 없는 비밀"
-        mvo.description = "여기서 너까지 다섯 걸음"
-        mvo.opendate = "2015-05-07"
-        mvo.rating = 9.19
-        
-        self.list.append(mvo)
     }
 
     // MARK: - Table view data source
@@ -72,18 +46,20 @@ class ListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return list.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let row = list[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell")!
+        cell.textLabel?.text = row.title
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("didSelectRowAt")
+    }
 
     /*
     // Override to support conditional editing of the table view.
