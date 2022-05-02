db1980c4c99624a1a785819248a729f203153828
kangddong <rkdehddud96@naver.com>
2022-05-02 23:26:51 +0900

    Chapter08 마무리

--- Chapter 08/Chapter 08/MovieCell.swift
+++ Chapter 08/Chapter 08/MovieCell.swift
@@ -14,35 +14,5 @@ class MovieCell: UITableViewCell {
     @IBOutlet weak var rating: UILabel!
     @IBOutlet weak var desc: UILabel!
     @IBOutlet weak var thumbnail: UIImageView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-        NSLog("awakeFromNib Called")
-    }
-    
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        
-        NSLog("prepareForReuse Called")
-    }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        
-        NSLog("layoutSubviews Called")
-    }
-    
-    override func layoutIfNeeded() {
-        super.layoutIfNeeded()
-        
-        NSLog("layoutIfNeeded Called")
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
